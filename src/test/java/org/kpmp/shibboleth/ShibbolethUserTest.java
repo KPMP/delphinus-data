286752cd75eeef5a16f66571a36680a7ea5aa1f4
Becky Reamy <rlreamy@umich.edu>
2019-08-05 11:45:11 -0400

    KPMP-1193: Added in code to get user information from Shibboleth.  Added logging service to log errors in a consistant manner

---
+++ src/test/java/org/kpmp/shibboleth/ShibbolethUserTest.java
@@ -0,0 +1,58 @@
+package org.kpmp.shibboleth;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShibbolethUserTest {
+
+	private ShibbolethUser testUser;
+
+	@Before
+	public void setUp() throws Exception {
+		testUser = new ShibbolethUser();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		testUser = null;
+	}
+
+	@Test
+	public void testSetFirstName() {
+		testUser.setFirstName("Ziggy");
+		assertEquals("Ziggy", testUser.getFirstName());
+	}
+
+	@Test
+	public void testSetLastName() {
+		testUser.setLastName("Stardust");
+		assertEquals("Stardust", testUser.getLastName());
+	}
+
+	@Test
+	public void testSetEmail() {
+		testUser.setEmail("ziggy@mars.com");
+		assertEquals("ziggy@mars.com", testUser.getEmail());
+	}
+
+	@Test
+	public void testSetDisplayName() {
+		testUser.setDisplayName("Space Oddity");
+		assertEquals("Space Oddity", testUser.getDisplayName());
+	}
+
+	@Test
+	public void testToString() {
+		testUser.setDisplayName("Space Oddity");
+		testUser.setFirstName("Ziggy");
+		testUser.setLastName("Stardust");
+		testUser.setEmail("ziggy@mars.com");
+		testUser.setShibId("ziggy@mars.com");
+		assertEquals("firstName: Ziggy" + ", lastName: Stardust" + ", displayName: Space Oddity"
+				+ ", email: ziggy@mars.com, shibId: ziggy@mars.com", testUser.toString());
+	}
+
+}
