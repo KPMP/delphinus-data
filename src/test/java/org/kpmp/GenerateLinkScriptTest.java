5410f50d5e8ce171b655e9d6d9d836b353aae206
Zach Wright <zwright@umich.edu>
2019-08-27 11:04:50 -0400

    KPMP-1226: Fix that test

--- src/test/java/org/kpmp/GenerateLinkScriptTest.java
+++ src/test/java/org/kpmp/GenerateLinkScriptTest.java
@@ -1,13 +1,32 @@
 package org.kpmp;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.kpmp.slides.ParticipantRepository;
 import org.kpmp.slides.Slide;
+import org.kpmp.slides.SlideService;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
 
 public class GenerateLinkScriptTest {
 
-    GenerateLinkScript generateLinkScript;
+    @Mock
+    private ParticipantRepository participantRepository;
+    private GenerateLinkScript generateLinkScript;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        generateLinkScript = new GenerateLinkScript(participantRepository);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        generateLinkScript = null;
+    }
 
     @Test
     public void testGetLinkConditional() {
@@ -15,10 +34,10 @@ public class GenerateLinkScriptTest {
         slide.setId("1234");
         slide.setSlideName("Slide_Name");
         String actual = generateLinkScript.getLinkConditional("P1", slide, "STAIN");
-        String expected  = "if ! [ -L P1_files ]; then\n" +
-                "    ln -s ../knowledgeEnvironment/deepZoom/files_P1_STAIN/{2}_files 1234_files\n" +
-                "    ln -s ../knowledgeEnvironment/deepZoom/files_P1_STAIN/{2}.dzi 1234.dzi\n" +
-                "    ln -s ../knowledgeEnvironment/deepZoom/files_P1_STAIN/tn_{2}.jpeg tn_1234.jpeg\n" +
+        String expected  = "if ! [ -L 1234_files ]; then\n" +
+                "    ln -s ../knowledgeEnvironment/deepZoom/files_P1_STAIN/Slide_Name_files 1234_files\n" +
+                "    ln -s ../knowledgeEnvironment/deepZoom/files_P1_STAIN/Slide_Name.dzi 1234.dzi\n" +
+                "    ln -s ../knowledgeEnvironment/deepZoom/files_P1_STAIN/tn_Slide_Name.jpeg tn_1234.jpeg\n" +
                 "fi \n";
         assertEquals(expected, actual);
     }
