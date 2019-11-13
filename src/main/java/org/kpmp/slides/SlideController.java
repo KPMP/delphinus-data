c5c26f5229ee892240c37d8fafc6478c0d60649f
Zach Wright <zwright@umich.edu>
2019-11-13 11:55:03 -0500

    KPMP-1417: Unused import

--- src/main/java/org/kpmp/slides/SlideController.java
+++ src/main/java/org/kpmp/slides/SlideController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
