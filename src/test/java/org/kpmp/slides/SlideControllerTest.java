8cfd2c37e16003baba7658787ab1a06790fe240e
Zach Wright <zwright@umich.edu>
2019-11-13 10:02:28 -0500

    KPMP-1417: Fix tests

--- src/test/java/org/kpmp/slides/SlideControllerTest.java
+++ src/test/java/org/kpmp/slides/SlideControllerTest.java
@@ -10,19 +10,24 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.kpmp.logging.LoggingService;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class SlideControllerTest {
 
 	@Mock
 	private SlideService slideService;
+	@Mock
+	private LoggingService loggingService;
 	private SlideController controller;
 
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		controller = new SlideController(slideService);
+		controller = new SlideController(slideService, loggingService);
 	}
 
 	@After
@@ -34,8 +39,8 @@ public class SlideControllerTest {
 	public void testGetSlidesForParticipant() {
 		List<Slide> slides = Arrays.asList(mock(Slide.class));
 		when(slideService.getSlidesForParticipant("444")).thenReturn(slides);
-
-		List<Slide> result = controller.getSlidesForParticipant("444");
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		List<Slide> result = controller.getSlidesForParticipant("444", request);
 
 		assertEquals(slides, result);
 	}
@@ -44,8 +49,8 @@ public class SlideControllerTest {
 	public void testGetAllSlides() {
 		List<Participant> slides = Arrays.asList(mock(Participant.class));
 		when(slideService.getAllParticipants()).thenReturn(slides);
-
-		List<Participant> result = controller.getAllParticipants();
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		List<Participant> result = controller.getAllParticipants(request);
 		assertEquals(slides, result);
 	}
 
